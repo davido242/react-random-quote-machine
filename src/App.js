04f580cc8668d7d7abfe61d3b2169fd9b3aa96d6
Monday_David_Sarka <david_md@hamari.agency>
2022-09-28 18:21:03 +0100

    Added an object to hold quotes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
-import './App.css';
+import React from "react";
+
+const quoteData = [
+  { text: "Hi Daveed, its been a while, you aiit?", author: "Joycee Rice" },
+  { text: "Belief creates the actual fact.", author: "William James" },
+  {
+    text: "It is during our darkest moments that we must focus to see the light.",
+    author: "Aristotle",
+  },
+  {
+    text: "Not having the best situation, but seeing the best in your situation is the key to happiness.",
+    author: "Marie Forleo",
+  },
+  {
+    text: "I am experienced enough to do this. I am knowledgeable enough to do this. I am prepared enough to do this. I am mature enough to do this. I am brave enough to do this.",
+    author: "Alexandria Ocasio-Cortez",
+  },
+];
 
 function App() {
   return (
